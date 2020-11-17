dd99c08d99727a175b3266ddab5b4f3feb2fddd0
Davi <dsales@procenge.com.br>
2020-11-17 20:41:22 -0300

    limpeza de código

--- src/app/signos/signo-exibir/signo-exibir.component.ts
+++ src/app/signos/signo-exibir/signo-exibir.component.ts
@@ -11,7 +11,6 @@ export class SignoExibirComponent implements OnInit {
 
   signo: Signo = new Signo();
   @Input() dataNascimento: Date;
-  @Output('signoEnviar') signoEmitir: EventEmitter<Signo> = new EventEmitter();
 
   constructor() { }
 
@@ -22,18 +21,10 @@ export class SignoExibirComponent implements OnInit {
 inicializa(){
   let valor: Date;
   valor = new Date(this.dataNascimento);
-  //console.log(valor);
-  //console.log("getDate", valor.getDate());
-  //console.log("getMonth",valor.getMonth());
     SIGNOS.forEach(value => {
-      console.log("value.dataInicial", value.dataInicial.toLocaleDateString());
-      console.log("valor",valor.toLocaleDateString());
-      console.log("value.dataFinal", value.dataFinal.toLocaleDateString());
-      console.log("\n=====================================================\n");
      if(value.dataInicial <= valor && value.dataFinal >= valor){
          this.signo = value;
       }
-      //console.log("Possivel signo do usuario", value.nome);
     })
 }
 
